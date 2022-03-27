b6895536161fe3694d939e9a8d42f95e80b6cd25
Fabrizio Zivolo <faz94@hotmail.it>
2022-03-27 14:40:24 +0200

    aggiunto commenti e ripulito

--- js/main.js
+++ js/main.js
@@ -1,17 +1,33 @@
-const hamburgerBTN = document.querySelector('.fas.fa-bars');
+/*
 
+A["selezionare il bottone nell'interfaccia (l'elemento con tre linee orizzontali)"] --> B[Creare una funzione]
+B --> C["Nella funzione: Selezionare l'elemento 'hamburgher-menu' "]
+C --> D["Nella funzione: applicare la classe 'active' all'elemento 'hamburgher-menu' "]
+D --> E["associare la funzione all'evento 'click' "]
+
+*/
+
+
+
+/* seleziono il bottone hamburger menu*/
+
+const hamburgerBTN = document.querySelector('.header-right > a');
+
+
+/* seleziono il bottone X */
+
+const crossBTN = document.querySelector('.close');
+
+/* funzione che seleziona elemento '.hamburger-menu' e gli applica classe 'active' se non presente e la rimuove se presente (toggle) */
 
 function toggleActive(){
     const menu = document.querySelector('.hamburger-menu');
     menu.classList.toggle('active')
-
 }
 
+/* associo la funzione all'evento 'click' ad entrambi gli elementi */
+
 hamburgerBTN.addEventListener('click', toggleActive)
-
-
-const crossBTN = document.querySelector('.close');
-
 
 crossBTN.addEventListener('click', toggleActive);
 
